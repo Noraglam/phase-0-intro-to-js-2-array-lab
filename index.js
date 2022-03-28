5dd667836712aaec6585a9c1c67b7962fae31ce1
Nora <noraonyi@gmail.com>
2022-03-27 22:39:07 -0400

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // Write your solution here!
+const cats= ("Milo", "Otis", "Garfield):
+
+function destructivelyAppendCat(name){
+    cats.push(name)
+    return(name)
+}
+
+
+function prependCat(){
+
+    const new Arr = ["Arnold"
+
+return new Arr
+
+}
+
+
+
+
+
+
